a393b79d45ea071bda4e975448128de38cadf725
Sarthak Anil <sarthakanil99@gmail.com>
2020-12-01 15:06:50 +0530

    404

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,22 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
+from flask import jsonify
+from flask import request
 from config import app
+from config import mysql
 
 
 @app.route('/')
 def hello_world():
     return 'Hello from Flask!'
 
+@app.errorhandler(404)
+def not_found(error=None):
+	message = {
+		'status': 404,
+		'message': 'Record not found: ' + request.url,
+	}
+	respone = jsonify(message)
+	respone.status_code = 404
+	return respone
+		
+if __name__ == "__main__":
+	app.run()
