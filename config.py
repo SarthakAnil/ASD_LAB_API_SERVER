bbd6d4ad89561eab406f7ca7e7990e98e047e599
Sarthak Anil <sarthakanil99@gmail.com>
2020-12-01 15:22:37 +0530

    fixed config

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from flaskext.mysql  import MySQL
 app = Flask(__name__)
 
 app.config['MYSQL_DATABASE_USER'] = 'sarthakanil'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Phoenix@777'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Qwerty@111'
 app.config['MYSQL_DATABASE_DB'] = 'sarthakanil$ocm'
 app.config['MYSQL_DATABASE_HOST'] = 'sarthakanil.mysql.pythonanywhere-services.com'
 
